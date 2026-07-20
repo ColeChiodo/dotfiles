57dfae18c98c65667270cca79b0c2860a7528305
ColeChiodo <colechiodo@gmail.com>
2026-07-20 15:42:05 -0700

    added keymap to nvim to type an arrow operator in one keymap

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -1,5 +1,8 @@
 local map = vim.keymap.set
 
+-- Type arrows with keymap
+map("i", "<C-l>", "->", { desc = "Insert ->" })
+
 -- Telescope
 map("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
 map("n", "<leader>fg", "<cmd>:Telescope live_grep<cr>")
